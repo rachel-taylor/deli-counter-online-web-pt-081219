616f8b573073b53ea4d669f7310d6a54eb7d0b86
Rachel T <rachmaybe@gmail.com>
2019-08-21 02:26:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,16 +1,16 @@
-def line(array)
-  if array.length >= 1 
-    new = []
-    counter = 1 
-    array.each do |name|
-      new.push("#{counter}. #{name}") 
-      counter += 1 
-    end 
-    puts "The line is currently: #{new_array.join(" ")}"
-  else 
-    puts "The line is currently empty."
-  end 
-end  
+# def line(array)
+#   if array.length >= 1 
+#     new = []
+#     counter = 1 
+#     array.each do |name|
+#       new.push("#{counter}. #{name}") 
+#       counter += 1 
+#     end 
+#     puts "The line is currently: #{new_array.join(" ")}"
+#   else 
+#     puts "The line is currently empty."
+#   end 
+# end  
 
 # def line_simple(array)
 #   current_line = "This line is currently:"

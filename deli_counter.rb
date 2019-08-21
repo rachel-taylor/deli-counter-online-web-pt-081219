fa595c8467da68caf15c6f6b29fe0a7bc01a2dd3
Rachel T <rachmaybe@gmail.com>
2019-08-21 02:30:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -32,7 +32,7 @@ def line(array)
   if array.length == 0 
     puts "The line is currently empty."
   else 
-    current_line = "The line is currently:"
+    message = "The line is currently:"
   array.each_with_index do |value, index| 
     message += " #{index.to_i+1}. #{value}"
   end 

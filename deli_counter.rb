6a766a51a34410d5b72fac59d8230bb940f10267
Rachel T <rachmaybe@gmail.com>
2019-08-21 02:33:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,28 +19,11 @@
 #   end 
 #   puts current_line
 # end 
-katz_deli = [] 
-
-def take_a_number(katz_deli, name)
+ def take_a_number(katz_deli, name)
   katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
-
-def line(array) 
-  if array.length == 0 
-    puts "The line is currently empty."
-  else 
-    message = "The line is currently:"
-  array.each_with_index do |value, index| 
-    message += " #{index.to_i+1}. #{value}"
-  end 
-  puts "#{message}"
-  end 
-end 
  
-line(katz_deli)
-
-
 def now_serving(array)
   if array.empty? == true 
     puts "There is nobody waiting to be served!"

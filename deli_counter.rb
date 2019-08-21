20634ab941b7487e075d3ad9cd5ffc6bc4117467
Rachel T <rachmaybe@gmail.com>
2019-08-21 02:19:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,9 +11,10 @@ def line(array)
     puts "The line is currently empty."
   end 
 end  
+
 def line_simple(array)
   current_line = "This line is currently:"
-  array.each.with_index(0) do |num, index|
+  array.each_with_index(0) do |num, index|
     current_line << "#{index}. #{num},"
   end 
   puts current_line

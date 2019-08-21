0d107242217302b866606a04d1dce2bf87ebb9c8
Rachel T <rachmaybe@gmail.com>
2019-08-21 02:34:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,9 @@
 #   end 
 #   puts current_line
 # end 
- def take_a_number(katz_deli, name)
+katz_deli = [] 
+
+def take_a_number(katz_deli, name)
   katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
